df9d5e44b6af1f1d6e689686718edafa9e3075cd
Theodore Pollock <49535454+PollockT@users.noreply.github.com>
2020-03-06 18:54:49 -0600

    starting point

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace HealthRecords
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            string fname;
+            string lname;
+            string gender;
+            double height;
+            double weight;
+            int birthyear;
+            int currentyear;
+
+            Console.Write("Enter client's first name: ");
+            fname = Console.ReadLine();
+            Console.Write("Enter the last name: ");
+            lname = Console.ReadLine();
+            Console.Write("Enter the client's gender:");
+            gender = Console.ReadLine();
+            Console.Write("What is the client's height in inches:");
+            height = Convert.ToDouble(Console.ReadLine());
+            Console.Write("What is the weight:");
+            weight = double.Parse(Console.ReadLine());
+            Console.Write("Enter Birth year:");
+            birthyear = int.Parse(Console.ReadLine());
+            Console.Write("Enter the current year:"); 
+            currentyear = int.Parse(Console.ReadLine());
+
+            client joe = new client(fname, lname, gender, height, weight, birthyear, currentyear);
+            joe.DisplayInfo(); 
+
+
+        }
+    }
+}
---
+++ client.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HealthRecords
+{
+    class client
+    {
+        //data 
+        private string fname;
+        private string lname;
+        private string gender;
+        private double height;
+        private double weight;
+        private int birthyear;
+        private int currentyear;
+
+        public client(string fname, string lname, string g, double h,
+
+            double w, int birthyear, int curryear)
+        {
+            //to refer to the instance variable use this 
+            this.fname = fname;
+            this.lname = lname;
+            gender = g;
+            this.height = h;
+            weight = w;
+            this.birthyear = birthyear;
+            currentyear = curryear;
+
+        }
+
+        public string getFname()
+        {
+            return fname;
+        }
+
+        public string getLname()
+        {
+            return lname; 
+
+        }
+
+
+        public string getGender ()
+        {
+            return gender; 
+        }
+
+        public double getHeight()
+        {
+            return height; 
+        }
+
+        public double getWeight()
+        {
+            return weight; 
+        }
+        public int getBirthYear()
+        {
+            return birthyear; 
+        }
+
+        public int getCurrentYear()
+        {
+            return currentyear; 
+        }
+        private double calculateBMI()
+        {
+            double bmi = 0.0;
+            bmi = (weight/Math.Pow(height,2) * 703) ; 
+            return bmi; 
+        }
+        public void DisplayInfo()
+        {
+            double bmi =  calculateBMI(); 
+            Console.WriteLine($"Health Record For: {fname} {lname}"); 
+        }
+
+
+
+    }//class 
+
+
+} //namespace 
